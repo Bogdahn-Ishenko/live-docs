f6f36daeb9815127540ac05cdf573dbaffe4c4b9
Bogdahn-Ishenko <ishenkobogdahn@gmail.com>
2026-04-15 05:53:36 +0300

    feat(yjs): add custom websocket server and integrate with docker compose

--- yjs-server/server.js
+++ yjs-server/server.js
@@ -1,16 +1,19 @@
 import http from 'http'
-import WebSocket from 'ws'
+import { WebSocketServer } from 'ws'
 import setupWSConnection from 'y-websocket/bin/utils.js'
 
-const port = process.env.PORT || 1234
+const PORT = process.env.PORT || 1234
 
 const server = http.createServer()
-const wss = new WebSocket.Server({ server })
+
+const wss = new WebSocketServer({
+  server
+})
 
 wss.on('connection', (conn, req) => {
   setupWSConnection(conn, req)
 })
 
-server.listen(port, () => {
-  console.log(`Yjs WebSocket server running on ws://localhost:${port}`)
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Yjs server running on ws://0.0.0.0:${PORT}`)
 })
